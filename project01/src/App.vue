f9e06f5706503f37a257e3cadda480384d1edf46
ahnyukyung <dksdbrud123@gmail.com>
2023-07-05 23:15:40 +0900

    v-if

--- project01/src/App.vue
+++ project01/src/App.vue
@@ -1,41 +1,60 @@
 <template>
+  <div class="modal_wrap" v-if="모달창 === true">
+    <button class="btn_close" @click="모달창 = false">닫기</button>
+    <div class="modal">
+      <h4>상세 페이지</h4>
+      <p>내용</p>
+    </div>
+  </div>
+
   <header id="header">
     <ul class="menu">
       <li v-for="menu in menus" :key="menu">
-        <a href="">{{ menu }}</a>
+        <a href>{{ menu }}</a>
       </li>
       <!-- <li><a href="">Products</a></li>
-      <li><a href="">About</a></li> -->
+      <li><a href="">About</a></li>-->
     </ul>
   </header>
-
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <main class="contents">
     <!-- <div v-for="product in products" :key="product">
       <h4 :style="red">{{ product }} 원룸</h4>
       <p>{{ prices[i] }}만원</p>
-    </div> -->
-    <button @click="increase">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }}</span>
-    <div>
-      <h4>{{ products[0] }} 원룸</h4>
+    </div>-->
+
+    <div class="item_box">
+      <img src="./assets/logo.png" alt="" />
+      <h4 @click="open">{{ products[0] }} 원룸</h4>
       <p>20만원</p>
-      <span>신고수 : {{ 신고수 }}</span>
+      <div class="btn_wrap">
+        <button @click="신고수[0]++">허위매물신고</button>
+        <span>신고수 : {{ 신고수[0] }}</span>
+      </div>
     </div>
-    <div>
+
+    <div class="item_box">
       <h4>{{ products[1] }} 원룸</h4>
       <p>30만원</p>
+      <div class="btn_wrap">
+        <button @click="increase">허위매물신고</button>
+        <span>신고수 : {{ 신고수[1] }}</span>
+      </div>
     </div>
-    <div>
+
+    <div class="item_box">
       <h4>{{ products[2] }} 원룸</h4>
       <p>40만원</p>
+      <div class="btn_wrap">
+        <button @click="increase">허위매물신고</button>
+        <span>신고수 : {{ 신고수[2] }}</span>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
+import onerooms from "./oneroom.js";
 
 export default {
   name: "App",
@@ -45,12 +64,17 @@ export default {
       red: "color : red",
       products: ["염창동", "천호동", "마포구"],
       prices: [10, 20, 30],
-      신고수: 0,
+      신고수: [0, 0, 0],
+      모달창: false,
+      원룸들: onerooms,
     };
   },
   methods: {
     increase() {
       this.신고수++;
+    },
+    open() {
+      this.모달창 = true;
     },
   },
   components: {
@@ -66,6 +90,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.modal_wrap {
+  position: absolute;
+  left: 50%;
+  top: 50px;
+  width: 80%;
+  min-height: 200px;
+  background-color: #df6565;
+  border-radius: 20px;
+  transform: translateX(-50%);
+}
+.modal_wrap .btn_close {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 #header {
 }
